ef43d9f08f44aef3a9b347811e18c96e29bc3b99
Elizabeth <elizabeth@ElizabethdeMacBook-Air.local>
2023-12-14 19:53:29 -0500

    commits for makeAppoinment

--- frontend/src/components/MakeAppointment/MakeAppointment.js
+++ frontend/src/components/MakeAppointment/MakeAppointment.js
@@ -14,13 +14,11 @@ function MakeAppointment() {
     setIsAppointmentMade(true);
   };
 
-  // Function to generate time slots, it could be dynamic based on the selected date
   const generateTimeSlots = () => {
     if (!appointmentDetails.date) {
       return <option value="">Please select a date first</option>;
     }
 
-    // Mocked time slots, these could be fetched from the server or generated based on availability
     return [
       <option value="9:00">9:00 AM</option>,
       <option value="10:00">10:00 AM</option>,
@@ -42,7 +40,7 @@ function MakeAppointment() {
             className="time-slot-input"
             value={appointmentDetails.timeSlot}
             onChange={(e) => setAppointmentDetails({ ...appointmentDetails, timeSlot: e.target.value })}
-            disabled={!appointmentDetails.date} // Disable if no date is selected
+            disabled={!appointmentDetails.date} 
           >
             <option value="">Select a time slot</option>
             {generateTimeSlots()}
@@ -50,7 +48,7 @@ function MakeAppointment() {
           <button 
             className="confirm-button" 
             onClick={handleConfirm}
-            disabled={!appointmentDetails.date || !appointmentDetails.timeSlot} // Disable if no date or time slot is selected
+            disabled={!appointmentDetails.date || !appointmentDetails.timeSlot}
           >
             Confirm
           </button>
